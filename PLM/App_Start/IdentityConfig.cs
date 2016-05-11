bf06fba73e0500f6efb044db39de4beea9c140e8
Brian <buchan28@mail.nmc.edu>
2016-05-11 13:46:44 -0400

    Changed password requirements-Alex olson

--- PLM/App_Start/IdentityConfig.cs
+++ PLM/App_Start/IdentityConfig.cs
@@ -29,7 +29,7 @@ namespace PLM
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
+                //RequireNonLetterOrDigit = true,
                 RequireDigit = true,
                 RequireLowercase = true,
                 RequireUppercase = true,
