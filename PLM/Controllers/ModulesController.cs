8d27b9444a9adfdb1969ae51514f20d569a53934
Brian <buchan28@mail.nmc.edu>
2016-05-11 14:49:36 -0400

    Categories Count-Alex Olson

--- PLM/Controllers/ModulesController.cs
+++ PLM/Controllers/ModulesController.cs
@@ -15,6 +15,62 @@ namespace PLM.Controllers
         {
             ModuleModel.Cats = db.Categories.ToList();
             ModuleModel.Mods = db.Modules.ToList();
+            //var query = (db.Modules
+            //            .GroupBy(p => new
+            //            {
+            //                p.CategoryId
+            //            })
+            //            .Select(g => new
+            //            {
+            //                g.Key.CategoryId,
+            //                Available = g.Count()
+            //            }));
+            //foreach(var x in ModuleModel.Cats)
+            ViewBag.Cat1Count = 
+                (from p in db.Modules
+             where p.CategoryId == 1
+             select p).Count();
+            ViewBag.Cat2Count =
+                (from p in db.Modules
+                 where p.CategoryId == 2
+                 select p).Count();
+            ViewBag.Cat3Count =
+                (from p in db.Modules
+                 where p.CategoryId == 3
+                 select p).Count();
+            ViewBag.Cat4Count =
+                (from p in db.Modules
+                 where p.CategoryId == 4
+                 select p).Count();
+            ViewBag.Cat5Count =
+                (from p in db.Modules
+                 where p.CategoryId == 5
+                 select p).Count();
+            ViewBag.Cat6Count =
+                (from p in db.Modules
+                 where p.CategoryId == 6
+                 select p).Count();
+            ViewBag.Cat7Count =
+                (from p in db.Modules
+                 where p.CategoryId == 7
+                 select p).Count();
+            ViewBag.Cat8Count =
+                (from p in db.Modules
+                 where p.CategoryId == 8
+                 select p).Count();
+           ViewBag.Cat9Count = 
+                (from p in db.Modules
+             where p.CategoryId == 9
+             select p).Count();
+            ViewBag.Cat10Count = 
+                (from p in db.Modules
+             where p.CategoryId == 10
+             select p).Count();
+            ViewBag.Cat11Count =
+                (from p in db.Modules
+                 where p.CategoryId == 11
+                 select p).Count();
+
             return View(ModuleModel);
         }
 
