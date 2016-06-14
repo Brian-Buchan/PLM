3ebb39e6f60e8b67e91e733a6bac3db033683cc6
Safaiaryu <jacquelineradtke@gmail.com>
2016-06-14 13:53:42 -0400

    Jacqueline - login fix

--- PLM/Controllers/AccountController.cs
+++ PLM/Controllers/AccountController.cs
@@ -355,14 +355,14 @@ namespace PLM.Controllers
                 var user = await UserManager.FindAsync(model.Email, model.Password);
                 if (user != null)
                 {
-                    if (!await UserManager.IsEmailConfirmedAsync(user.Id))
-                    {
-                        string callbackUrl = await SendEmailConfirmationTokenAsync(user.Id, "Confirm your account-Resend");
+                    //if (!await UserManager.IsEmailConfirmedAsync(user.Id))
+                    //{
+                        //string callbackUrl = await SendEmailConfirmationTokenAsync(user.Id, "Confirm your account-Resend");
                         ViewBag.errorMessage = "You must have a confirmed email to log on.";
-                        return View("Error");
-                        //await SignInAsync(user, model.RememberMe);
-                        //return RedirectToLocal(returnUrl);
-                    }
+                        //return View("Error");
+                        await SignInAsync(user, model.RememberMe);
+                        return RedirectToLocal(returnUrl);
+                    //}
                 }
                 else
                 {
@@ -395,23 +395,23 @@ namespace PLM.Controllers
                 IdentityResult result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
-                    await SignInAsync(user, isPersistent: false);
+                    //await SignInAsync(user, isPersistent: false);
 
                     // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
-                    string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
-                    var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-                    await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+                    //string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
+                    //var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+                    //await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
                     UserManager.AddToRole(user.Id, "User");
 
-                    callbackUrl = await SendEmailConfirmationTokenAsync(user.Id, "Confirm your account");
+                    //callbackUrl = await SendEmailConfirmationTokenAsync(user.Id, "Confirm your account");
 
                     ViewBag.Message = "Check your email and confirm your account, you must be confirmed "
                         + "before you can log in.";
 
-                    return View("Info");
-                    //return RedirectToAction("Index", "Home");
+                    //return View("Info");
+                    return RedirectToAction("Index", "Home");
                 }
                 else
                 {
--- PLM/Models/EmailService.cs
+++ PLM/Models/EmailService.cs
@@ -24,7 +24,7 @@ namespace PLM.Models
             var myMessage = new SendGridMessage();
             myMessage.AddTo(message.Destination);
             myMessage.From = new System.Net.Mail.MailAddress(
-                                "Joe@contoso.com", "Joe S.");
+                                "radtke18@mail.nmc.edu", "Mark and Tom");
             myMessage.Subject = message.Subject;
             myMessage.Text = message.Body;
             myMessage.Html = message.Body;
