75825c834e50a528d952964642c01cbeb071bb9f
proudfb <bproudft@gmail.com>
2016-06-28 15:38:56 -0400

    Ben - Added function to dispose of old game session on quit + changes to image editor testbed

---
+++ PLM/Models/ConfirmViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace PLM.Models
+{
+    public class ConfirmViewModel
+    {
+        /// <summary>
+        /// Base 64 image data
+        /// </summary>
+        public string Base64ImgData { get; set; }
+        /// <summary>
+        /// The original image's url
+        /// </summary>
+        public string originalUrl { get; set; }
+
+        public ConfirmViewModel(string b64Img, string origUrl)
+        {
+            Base64ImgData = b64Img;
+            originalUrl = origUrl;
+        }
+    }
+}
