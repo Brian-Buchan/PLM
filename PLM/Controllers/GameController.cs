2e20dfaa2acd11fbd64c84ae05f3730c5410a744
YatoSamaSensei <Olson25@mail.nmc.edu>
2016-06-28 13:04:09 -0400

    Fixed x/y questions

--- PLM/Controllers/GameController.cs
+++ PLM/Controllers/GameController.cs
@@ -68,7 +68,7 @@ namespace PLM.Controllers
         {
             //Update the user's score and time
             ((UserGameSession)Session["userGameSession"]).Score = Score;
-            ViewBag.Progress = ((UserGameSession)Session["userGameSession"]).currentQuestion;
+            ViewBag.Progress = ((UserGameSession)Session["userGameSession"]).currentQuestion + 1;
             ViewBag.TotalQuestions = ((UserGameSession)Session["userGameSession"]).numQuestions;
             ((UserGameSession)Session["userGameSession"]).timeLeft = TimeSpan.Parse(Time);
             if (IsGameDone())
