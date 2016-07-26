c06b0966d94792778395133c9192bcaf99b5175b
proudfb <bproudft@gmail.com>
2016-07-26 13:09:40 -0400

    Ben - Reverted error sending stuff

--- PLM/Controllers/PicturesController.cs
+++ PLM/Controllers/PicturesController.cs
@@ -217,16 +217,16 @@ namespace PLM.Controllers
 
             string imgId = Request.Form.Get("imgId");
             string answerId = Request.Form.Get("answerId");
-            string origUrl = Request.Form.Get("origUrl");
+            //string origUrl = Request.Form.Get("origUrl");
             string imgData = Request.Form.Get("imgData");
-            string imageFormat = "." + imgData.Substring(imgData.IndexOf('/') + 1, imgData.IndexOf(';'));
+            //string imageFormat = "." + imgData.Substring(imgData.IndexOf('/') + 1, imgData.IndexOf(';'));
 
-            if (imageFormat != Path.GetExtension(origUrl))
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.InternalServerError,
-                    "The selected image format is not the same as the original image format." +
-                    "\nPlease select the other image format.");
-            }
+            //if (imageFormat != Path.GetExtension(origUrl))
+            //{
+            //    return new HttpStatusCodeResult(HttpStatusCode.InternalServerError,
+            //        "The selected image format is not the same as the original image format." +
+            //        "\nPlease select the other image format.");
+            //}
 
             string result = SaveImage(imgData, imgId, answerId);
             
