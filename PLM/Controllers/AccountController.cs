23f489dccf8e39768cd9207d63b140c4343059f4
Safaiaryu <jacquelineradtke@gmail.com>
2016-07-26 14:21:03 -0400

    jacqueline - revert edit account

--- PLM/Controllers/AccountController.cs
+++ PLM/Controllers/AccountController.cs
@@ -234,7 +234,7 @@ namespace PLM.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         //[AuthorizeOrRedirectAttribute(Roles = "Admin")]
-        public ActionResult Edit([Bind(Include = "UserName, LastName, FirstName, Institution, Email, Status")] EditUserViewModel userModel)
+        public ActionResult Edit([Bind(Include = "UserName, LastName, FirstName, Institution, Email, Status, Password, ConfirmPassword")] EditUserViewModel userModel)
         {
             if (ModelState.IsValid)
             {
