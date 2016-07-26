693647ab0d810215a27d64489ef0f7212dc63bc9
YatoSamaSensei <Olson25@mail.nmc.edu>
2016-07-26 12:56:51 -0400

    probly fixed

--- PLM/Extensions/Extensions.cs
+++ PLM/Extensions/Extensions.cs
@@ -35,8 +35,8 @@ namespace PLM
         {
             ApplicationDbContext db = new ApplicationDbContext();
             List<Score> scores = db.Scores.Where(x => x.ModuleID == moduleID).ToList();
-            scores.OrderBy(x => (x.CorrectAnswers / x.TotalAnswers));
-            scores = scores.Take(10).ToList();
+            scores = scores.OrderBy(x => (x.CorrectAnswers / x.TotalAnswers)).Take(10).ToList();
+            //scores = scores.Take(10).ToList();
             //InvalidCast error
             //Unable to cast object of type '<TakeIterator>d__3a`1[PLM.Score]' to type 'System.Collections.Generic.List`1[PLM.Score]'.
             return(scores);
