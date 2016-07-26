d9b717b03e6025bb1f13d4e02dea7b88deca141d
proudfb <bproudft@gmail.com>
2016-07-26 13:32:12 -0400

    Ben - Added fix for image editor, now saves as jpg instead of jpeg, which was causing problems. Now will try to make image extensions consistent

--- PLM/Controllers/PicturesController.cs
+++ PLM/Controllers/PicturesController.cs
@@ -345,6 +345,12 @@ namespace PLM.Controllers
 
                 //gets the image format from the post
                 string imageFormat = imageBase64.Substring(imageBase64.IndexOf('/') + 1, imageBase64.IndexOf(';') - 11);
+                
+                //If the image format is jpeg (which will break the system), 
+                if (imageFormat == "jpeg")
+                {
+                    imageFormat = "jpg";
+                }
 
                 //gets the file data as a Base64 string
                 imageBase64 = imageBase64.Substring(imageBase64.LastIndexOf(',') + 1);
