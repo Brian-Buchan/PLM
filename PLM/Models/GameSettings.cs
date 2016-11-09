6bc1e5c49f1fa77c503fed6f6525f82c3f2e229f
Brian Buchan <buchan28@mail.nmc.edu>
2016-11-09 18:23:36 -0500

    add game settings

---
+++ PLM/Models/GameSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PLM
+{
+    public class GameSettings
+    {
+        public int Time { get; set; }
+        public int Questions { get; set; }
+        public int Answers { get; set; }
+    }
+}
