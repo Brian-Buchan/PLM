8985ed12df83ec2456c53dc5477e51f377ad3608
buchan28 <buchan28@PLM-MGT>
2017-01-11 16:59:47 -0500

    f

--- PLM/DevPro Resources/DevPro.cs
+++ PLM/DevPro Resources/DevPro.cs
@@ -7,14 +7,14 @@ namespace PLM
 {
     public static class DevPro
     {
-        public static string connectionStringName = "Azure";
-        public static string baseFileDirectory = "NeedToRemoveFeature";
+        //public static string connectionStringName = "Azure";
+        //public static string baseFileDirectory = "NeedToRemoveFeature";
 
         //public static string connectionStringName = "Development";
         //public static string baseFileDirectory = "/PerceptualLearningDevelopment/Content/Images/";
 
-        //public static string connectionStringName = "Production";
-        //public static string baseFileDirectory = "/PerceptualLearning/Content/Images/";
+        public static string connectionStringName = "Production";
+        public static string baseFileDirectory = "/PerceptualLearning/Content/Images/";
 
         //CHANGE IN: ImageEditorProd.js
         //In Scripts Folder
